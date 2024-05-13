c32f8a2f533bd5c8f197ad931757ba2e6f052ffa
denniskabatha <162292636+denniskabatha@users.noreply.github.com>
2024-05-13 03:41:11 +0300

    Create App.js

---
+++ car-care/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from './componets/Navbar'
+
+import Contacts from './componets/Contacts'
+import HomeSection from './componets/HomeSection'
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <HomeSection/>
+      <Contacts/>
+    </div>
+  )
+}
+
+export default App
+
